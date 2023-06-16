f4bea14bb4f77d231e57eaa9617602f8336fad8c
Ishu Gaur <Officialishu07@gmail.com>
2023-06-16 18:21:01 +0530

    6_20_commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -16,12 +20,52 @@ public class Main {
         System.out.println();
 
         Dish.menu(restro);
-        Dish.displayMenu("C:\\Users\\DELL\\IdeaProjects\\Sharda_Swiggy\\src\\main\\java\\org\\example\\Menu.csv");
+        Dish.displayMenu("C:\\Users\\DELL\\IdeaProjects\\Sharda_Swiggy\\src\\main\\java\\org\\example\\  Menu.csv");
 
         System.out.println("\n"+"Choose Dishes you want to eat by their Dish Id");
+        scanner.nextLine();
+        String input = scanner.nextLine();
+        String[] dishIdsArray = input.split(",");
+        Map<Integer, Integer> dishQuantities = new HashMap<>();
 
+        for (String dishId : dishIdsArray) {
+            int id = Integer.parseInt(dishId.trim());
+            System.out.printf("Enter quantity for dish ID %d: ", id);
+            int quantity = scanner.nextInt();
+            dishQuantities.put(id, quantity);
+        }
 
+        double bill = calculateBill(restro, dishQuantities);
+        System.out.printf("\nYour total bill is: %.2f\n", bill);
 
         scanner.close();
+    }
+
+    public static double calculateBill(int restaurantId, Map<Integer, Integer> dishQuantities) {
+        double totalBill = 0.0;
+        String[][] menuData = Dish.displayMenu("C:\\Users\\DELL\\IdeaProjects\\Sharda_Swiggy\\src\\main\\java\\org\\example\\Menu.csv");
+
+        if (menuData == null) {
+            System.out.println("Failed to read menu data.");
+            return totalBill;
+        }
+
+        System.out.println("\nCalculating bill...\n");
+
+        for (String[] row : menuData) {
+            int currRestaurantId = Integer.parseInt(row[0]);
+            int dishId = Integer.parseInt(row[1]);
+            String dishName = row[2];
+            double dishPrice = Double.parseDouble(row[3]);
+
+            if (currRestaurantId == restaurantId && dishQuantities.containsKey(dishId)) {
+                int quantity = dishQuantities.get(dishId);
+                double itemTotal = dishPrice * quantity;
+                System.out.printf("Dish: %-10s Quantity: %d Price: %.2f\n", dishName, quantity, itemTotal);
+                totalBill += itemTotal;
+            }
+        }
+
+        return totalBill;
     }
 }
